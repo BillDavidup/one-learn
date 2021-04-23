c81c9bc3f50f68845f24211b350b1b0d110546c5
qiaoni <2765887346@qq.com>
2021-04-23 19:01:15 +0800

    项目初始化

---
+++ src/main/java/com/qiaoni/learn/onelearn/OneLearnApplication.java
@@ -0,0 +1,13 @@
+package com.qiaoni.learn.onelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiaoni/learn/onelearn/OneLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaoni.learn.onelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
